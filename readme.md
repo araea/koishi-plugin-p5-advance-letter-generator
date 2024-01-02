c98c70c994d1311af95a75a48fd479073be8b2e7
Nawyjx <3373167460@qq.com>
2024-01-02 16:10:01 +0800

    Update

--- readme.md
+++ readme.md
@@ -2,4 +2,70 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-p5-advance-letter-generator?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-p5-advance-letter-generator)
 
+## 🎈 介绍
 
+这是一个基于 [Koishi](https://koishi.chat/) 的插件，可以让你的机器人生成 **p5 预告信** 和 **p5 UI** 的图片，让你的群聊更加有趣和热闹！🔥
+
+p5 预告信是指《女神异闻录5》中的一种信件，由佐仓双叶用特殊的字体和颜色写成，用来告知主角和同伴们下一次的目标和计划。💌
+
+p5 UI 是指《女神异闻录5》中的一种用户界面，由鲜艳的红色和白色组成，具有强烈的视觉冲击力和动感。🎨
+
+这个插件可以让你自定义要生成的文本内容，然后用随机的字体、颜色、大小和旋转角度来模拟 p5 预告信和 p5 UI 的效果，让你感受到佐仓双叶的魅力和 p5 的风格。😍
+
+## 📦 安装
+
+```bash
+npm install koishi-plugin-p5-advance-letter-generator
+```
+
+或者，你可以直接前往 Koishi 插件市场添加该插件。👍
+
+## 🎮 使用
+
+- 你需要先安装 [koishi-plugin-canvas](https://github.com/koishijs/koishi-plugin-canvas) 插件（必须用这个），以便使用画布功能。🖼️
+- 在 `Koishi` 默认根目录下，安装 `./data/p5alg/` 文件夹内的 5 个字体。🛠️
+- 建议为指令添加合适的别名，以便你使用该插件提供的命令，来生成 p5 预告信和 p5 UI 的图片。📸
+
+## ⚙️ 配置项
+
+该插件目前没有提供任何配置项，但是你可以在命令中指定一些参数，如画布的宽度和高度。📏
+
+## 📝 命令
+
+该插件提供了两个命令，分别是：
+
+- `p5advanceLetter`：查看 p5 预告信生成帮助。📚
+- `p5advanceLetter.generateAdvanceLetter [text:text]`：生成 p5 预告信。💌
+- `p5advanceLetter.generateUI [text:text]`：生成 p5 UI。🎨
+
+其中，`[text:text]` 参数是必须的，表示要生成的文本内容。你可以用 `/` 来换行。🔤
+
+你还可以使用以下选项来调整生成的图片：
+
+- `-w [canvasWidth:number]`：画布宽度，默认为 1770 像素。🔢
+- `--height [canvasHeight:number]`：画布高度，默认为 1300 像素。🔢
+
+例如，你可以输入以下命令：
+
+```bash
+p5advanceLetter.generateAdvanceLetter -w 1920 --height 1080 尊敬的金城润矢先生:/扭曲事实沉溺于金钱利益之人 ，/您的种种恶行，我等已全然知晓/那个扭曲的欲望/就由我等来收下!/心之怪盗团-Joker敬上
+```
+
+来生成一张宽为 1920 像素，高为 1080 像素的 p5 预告信图片，示例如下：
+
+![p5预告信示例](https://cdn.nlark.com/yuque/0/2024/png/34538662/1704182269504-8a68ca1a-0b48-4525-b57f-372d42706928.png?x-oss-process=image%2Fresize%2Cw_937%2Climit_0)
+
+p5 UI 图片，示例如下：
+
+![p5UI示例](https://cdn.nlark.com/yuque/0/2024/png/34538662/1704182269522-dabdd1ef-49a2-4a74-a838-392c3112aee7.png?x-oss-process=image%2Fresize%2Cw_937%2Climit_0)
+
+
+## 🙏 致谢
+
+* [Koishi](https://koishi.chat/) - 机器人框架
+* [女神异闻录5](https://www.jp.playstation.com/games/persona-5-royal/) - 一款优秀的角色扮演游戏
+* [nonebot-plugin-p5generator](https://github.com/xi-yue-233/nonebot-plugin-p5generator) - 优秀的插件代码库
+
+## 📄 License
+
+MIT License © 2023
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,335 @@
-import { Context, Schema } from 'koishi'
+import { Context, Logger, Schema, h } from 'koishi'
 
+
+import * as fs from 'fs';
+import * as path from 'path';
+import { randomBytes } from 'crypto';
+import { } from 'koishi-plugin-canvas'
+
+export const inject = ['canvas']
 export const name = 'p5-advance-letter-generator'
+export const usage = `## 🎮 使用
 
-export interface Config {}
+- 你需要先安装 [koishi-plugin-canvas](https://github.com/koishijs/koishi-plugin-canvas) 插件（必须用这个），以便使用画布功能。🖼️
+- 在 \`Koishi\` 默认根目录下，安装 \`./data/p5alg/\` 文件夹内的 5 个字体。🛠️
+- 建议为指令添加合适的别名，以便你使用该插件提供的命令，来生成 p5 预告信和 p5 UI 的图片。📸
+
+## ⚙️ 配置项
+
+该插件目前没有提供任何配置项，但是你可以在命令中指定一些参数，如画布的宽度和高度。📏
+
+## 📝 命令
+
+该插件提供了两个命令，分别是：
+
+- \`p5advanceLetter\`：查看 p5 预告信生成帮助。📚
+- \`p5advanceLetter.generateAdvanceLetter [text:text]\`：生成 p5 预告信。💌
+- \`p5advanceLetter.generateUI [text:text]\`：生成 p5 UI。🎨
+
+其中，\`[text:text]\` 参数是必须的，表示要生成的文本内容。你可以用 \`/\` 来换行。🔤
+
+你还可以使用以下选项来调整生成的图片：
+
+- \`-w [canvasWidth:number]\`：画布宽度，默认为 1770 像素。🔢
+- \`--height [canvasHeight:number]\`：画布高度，默认为 1300 像素。🔢
+
+例如，你可以输入以下命令：
+
+\`\`\`bash
+p5advanceLetter.generateAdvanceLetter -w 1920 --height 1080 尊敬的金城润矢先生:/扭曲事实沉溺于金钱利益之人 ，/您的种种恶行，我等已全然知晓/那个扭曲的欲望/就由我等来收下!/心之怪盗团-Joker敬上
+\`\`\`
+
+来生成一张宽为 1920 像素，高为 1080 像素的 p5 预告信图片，示例如下：
+
+![p5预告信示例](https://cdn.nlark.com/yuque/0/2024/png/34538662/1704182269504-8a68ca1a-0b48-4525-b57f-372d42706928.png?x-oss-process=image%2Fresize%2Cw_937%2Climit_0)
+
+p5 UI 图片，示例如下：
+
+![p5UI示例](https://cdn.nlark.com/yuque/0/2024/png/34538662/1704182269522-dabdd1ef-49a2-4a74-a838-392c3112aee7.png?x-oss-process=image%2Fresize%2Cw_937%2Climit_0)`
+
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
+const logger = new Logger(`p5AdvanceLetterGenerator`)
 export function apply(ctx: Context) {
-  // write your plugin here
+  const dependencyPjskDir = path.join(__dirname, '..', 'p5alg')
+  const pluginDataDir = path.join(ctx.baseDir, 'data', 'p5alg')
+  // 将资源文件存到data目录下
+  fs.cp(dependencyPjskDir,
+    pluginDataDir,
+    { recursive: true, force: false },
+    (err) => {
+      if (err) {
+        logger.error('复制 p5alg 文件夹出错：' + err.message)
+      }
+    });
+  ctx.command('p5advanceLetter', '查看p5预告信生成帮助')
+    .action(async ({ session }) => {
+      await session.execute(`p5advanceLetter -h`)
+    })
+  ctx.command('p5advanceLetter.generateAdvanceLetter [text:text]', '生成p5预告信')
+    .option('canvasWidth', '-w [canvasWidth:number] 画布宽度', { fallback: 1770 })
+    .option('canvasHeight', '--height [canvasHeight:number] 画布高度', { fallback: 1300 })
+    .action(async ({ session, options }, text) => {
+      if (!text) {
+        await session.send('请给我要生成的语句呀，不然佐仓双叶来了都没办法生成！')
+        return
+      }
+      const { canvasWidth, canvasHeight } = options
+      text = text.replace(/\/+/g, '\n');
+      const result = await generateAdvanceLetterImage(text, canvasWidth, canvasHeight)
+      await session.send(h.image(result, 'image/png'));
+    })
+  ctx.command('p5advanceLetter.generateUI [text:text]', '生成p5UI')
+    .option('canvasWidth', '-w [canvasWidth:number] 画布宽度', { fallback: 1770 })
+    .option('canvasHeight', '--height [canvasHeight:number] 画布高度', { fallback: 1300 })
+    .action(async ({ session, options }, text) => {
+      if (!text) {
+        await session.send('请给我要生成的语句呀，不然佐仓双叶来了都没办法生成！')
+        return
+      }
+      const { canvasWidth, canvasHeight } = options
+      text = text.replace(/\/+/g, '\n');
+      const result = await generateUIImage(text, canvasWidth, canvasHeight)
+      await session.send(h.image(result, 'image/png'));
+    })
+
+
+
+  async function generateAdvanceLetterImage(text: string, canvasWidth: number, canvasHeight: number): Promise<Buffer> {
+    // 设置字体列表
+    const fonts: string[] = ['微软雅黑', '微软雅黑 Bold', '黑体', '新宋体', '华文琥珀'];
+    // 设置颜色列表
+    const colors: string[] = ['white', 'black', 'gray', 'red'];
+    // 设置背景颜色列表
+    const bgColors: { [key: string]: string[] } = {
+      white: ['black', 'gray'],
+      black: ['white', 'gray'],
+      gray: ['white', 'black'],
+      red: ['white'],
+    };
+    const canvas = await ctx.canvas.createCanvas(canvasWidth, canvasHeight);
+    const context = canvas.getContext('2d');
+
+    // 加载背景图片
+    const backgroundImagePath = path.join(pluginDataDir, 'background.png');
+    const backgroundImage = await ctx.canvas.loadImage(backgroundImagePath);
+    context.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
+
+    // 分割文本为多个句子
+    const sentences = text.split('\n');
+
+    // 计算文本总高度和最大字符高度
+    let textHeight = 0;
+    let maxCharHeight = 0;
+    const sentencesCount = sentences.length;
+    for (const sentence of sentences) {
+      const trimmedSentence = sentence.trim();
+      const textFont = randomChoice(fonts);
+      const textSize = Math.min(1770 / trimmedSentence.length, 1300 / (sentencesCount + 6));
+      context.font = (textFont === '微软雅黑 Bold') ? `bold ${textSize}px "${textFont}"` : `${textSize}px "${textFont}`;
+
+      const metrics = context.measureText(trimmedSentence);
+      const charHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+      maxCharHeight = Math.max(maxCharHeight, charHeight);
+
+      textHeight += maxCharHeight + 20;
+    }
+
+    // 计算文本起始位置
+    let y = (canvasHeight - textHeight) / 2;
+
+    // 遍历每个句子
+    for (const sentence of sentences) {
+      // 计算句子宽度
+      const trimmedSentence = sentence.trim();
+      const textSize = Math.min(1770 / (trimmedSentence.length + 6), 1300 / (sentencesCount + 6));
+      const textFont = randomChoice(fonts);
+      context.font = (textFont === '微软雅黑 Bold') ? `bold ${textSize}px "${textFont}"` : `${textSize}px "${textFont}`;
+
+      const metrics = context.measureText(trimmedSentence);
+      const sentenceWidth = metrics.width;
+
+      // 计算句子起始位置
+      let x = (canvasWidth - sentenceWidth) / 2 - 170;
+      // 遍历句子中的每个字符
+      for (let i = 0; i < trimmedSentence.length; i++) {
+        const char = trimmedSentence[i];
+
+        // 随机选择字体和大小
+        const charFont = randomChoice(fonts);
+        const charSize = Math.floor(textSize + (randomBytes(1)[0] / 255) * 20);
+        context.font = (charFont === '微软雅黑 Bold') ? `bold ${charSize}px "${charFont}"` : `${charSize}px "${charFont}`;
+
+
+        // 随机选择颜色
+        const color = randomChoice(colors);
+        // 根据颜色选择背景颜色
+        const bgColor = randomChoice(bgColors[color]);
+
+        // 获取字符大小
+        const charMetrics = context.measureText(char);
+        const charWidth = charMetrics.width;
+        const charHeight = charMetrics.actualBoundingBoxAscent + charMetrics.actualBoundingBoxDescent;
+
+        // 计算字符中心点坐标
+        const centerX = x + charWidth / 2;
+        const centerY = y + charHeight / 2;
+
+        // 随机旋转角度
+        const randomAngle = (randomBytes(4).readUInt32BE(0) / 0xffffffff) * 10 - 5;
+        const angle = randomAngle * Math.PI / 180;
+
+        // 移动画布原点到字符中心点
+        context.translate(centerX, centerY);
+        // 进行旋转
+        context.rotate(angle);
+        // 移动画布原点回初始位置
+        context.translate(-centerX, -centerY);
+
+        // 绘制背景色块
+        context.fillStyle = bgColor;
+        context.fillRect(x, y, charWidth, charHeight);
+
+        // 绘制字符
+        context.fillStyle = color;
+        context.fillText(char, x, y + charMetrics.actualBoundingBoxAscent);
+
+        // 更新x坐标
+        x += charWidth + 20;
+
+        // 恢复旋转
+        context.resetTransform();
+      }
+      // 更新y坐标以换行
+      y += maxCharHeight + 20;
+    }
+
+    // 将画布转换为图像数据
+    const result = await canvas.toBuffer('image/png');
+
+    return result;
+  }
+  async function generateUIImage(text: string, canvasWidth: number, canvasHeight: number): Promise<Buffer> {
+    // 设置字体列表
+    const fonts = ['微软雅黑 Bold'];
+    // 设置颜色列表
+    const colors = ['white', 'red'];
+    // 设置背景颜色列表
+    const bgColors: { [key: string]: string[] } = { white: ['black', 'red'], red: ['black'] };
+
+    const canvas = await ctx.canvas.createCanvas(canvasWidth, canvasHeight);
+    const context = canvas.getContext('2d');
+
+    // 加载背景图片
+    const backgroundImagePath = path.join(pluginDataDir, 'background.png');
+    const backgroundImage = await ctx.canvas.loadImage(backgroundImagePath);
+    context.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
+
+    // 分割文本为多个句子
+    const sentences = text.split('\n');
+
+    // 计算文本总高度和最大字符高度
+    let textHeight = 0;
+    let maxCharHeight = 0;
+    const sentencesCount = sentences.length;
+    for (const sentence of sentences) {
+      const trimmedSentence = sentence.trim();
+      const textSize = Math.min(1770 / trimmedSentence.length, 1300 / (sentencesCount + 6));
+      context.font = `bold ${textSize}px "${textSize}"`;
+
+      const metrics = context.measureText(trimmedSentence);
+      const charHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+      maxCharHeight = Math.max(maxCharHeight, charHeight);
+
+      textHeight += maxCharHeight + 20;
+    }
+
+    // 计算文本起始位置
+    let y = (canvasHeight - textHeight) / 2;
+
+    // 遍历每个句子
+    for (const sentence of sentences) {
+      // 计算句子宽度
+      const trimmedSentence = sentence.trim();
+      const textSize = Math.min(1770 / (trimmedSentence.length + 6), 1300 / (sentencesCount + 6));
+      const textFont = randomChoice(fonts);
+      context.font = `bold ${textSize}px "${textFont}"`;
+
+      const metrics = context.measureText(trimmedSentence);
+      const sentenceWidth = metrics.width;
+
+      // 计算句子起始位置
+      let x = (canvasWidth - sentenceWidth) / 2 - 170;
+      // 遍历句子中的每个字符
+      for (let i = 0; i < trimmedSentence.length; i++) {
+        const char = trimmedSentence[i];
+
+        // 随机选择字体和大小
+        const charFont = randomChoice(fonts);
+        const charSize = Math.floor(textSize + (randomBytes(1)[0] / 255) * 20);
+        context.font = `bold ${charSize}px "${charFont}"`;
+
+        // 随机选择颜色
+        const color = randomChoice(colors);
+        // 根据颜色选择背景颜色
+        const bgColor = randomChoice(bgColors[color]);
+
+        // 获取字符大小
+        const charMetrics = context.measureText(char);
+        const charWidth = charMetrics.width;
+        const charHeight = charMetrics.actualBoundingBoxAscent + charMetrics.actualBoundingBoxDescent;
+
+        // 计算字符中心点坐标
+        const centerX = x + charWidth / 2;
+        const centerY = y + charHeight / 2;
+
+        // 随机旋转角度
+        const randomAngle = (randomBytes(4).readUInt32BE(0) / 0xffffffff) * 10 - 5;
+        const angle = randomAngle * Math.PI / 180;
+
+        // 移动画布原点到字符中心点
+        context.translate(centerX, centerY);
+        // 进行旋转
+        context.rotate(angle);
+        // 移动画布原点回初始位置
+        context.translate(-centerX, -centerY);
+
+        // 绘制背景色块 
+        context.fillStyle = bgColor;
+        context.fillRect(x, y, charWidth + 7, charHeight + 7);
+
+        // 绘制白色边框
+        context.strokeStyle = 'white';
+        context.lineWidth = 7;
+        context.strokeRect(x, y, charWidth, charHeight);
+
+        // 绘制字符
+        context.fillStyle = color;
+        context.fillText(char, x + 7, y + 7 + charMetrics.actualBoundingBoxAscent);
+
+
+        // 更新x坐标
+        x += charWidth + 20;
+
+        // 恢复旋转
+        context.resetTransform();
+      }
+      // 更新y坐标以换行
+      y += maxCharHeight + 20;
+    }
+
+    // 将画布转换为图像数据
+    const result = await canvas.toBuffer('image/png');
+
+    return result;
+  }
+
+  function randomChoice<T>(array: T[]): T {
+    const randomBytesBuffer = randomBytes(4);
+    const index = randomBytesBuffer.readUInt32BE(0) % array.length;
+    return array[index];
+  }
 }
+
